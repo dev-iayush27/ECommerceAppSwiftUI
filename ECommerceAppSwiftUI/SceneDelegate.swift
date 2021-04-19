d9949abe72bd31fcc513f960e685c91c6fcb883d
Ayush Gupta <ayush.gupta2@cognizant.com>
2021-04-19 11:28:25 +0530

    Set root navigation.

--- ECommerceAppSwiftUI/SceneDelegate.swift
+++ ECommerceAppSwiftUI/SceneDelegate.swift
@@ -22,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: LogInView())
+            window.rootViewController = UIHostingController(rootView: TabBarView())
             self.window = window
             window.makeKeyAndVisible()
         }
